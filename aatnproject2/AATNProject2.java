cd13eb8a1d41984088f79acd53951ec3a58735ec
ssdeep <saideep.vn765@gmail.com>
2014-07-19 12:58:46 -0500

    Initialize Project

---
+++ aatnproject2/AATNProject2.java
@@ -0,0 +1,23 @@
+/*
+ * This source code is designed and written by Saideep Sambaraju
+ * It is to be used for non commercial purposes only
+ * DO NOT copy this code without permission from the author
+ * Contact Info : 408-203-0492
+ */
+
+package aatnproject2;
+
+/**
+ *
+ * @author esamsai
+ */
+public class AATNProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
